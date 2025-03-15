b8042ea6b9796c5e679a160abefbff678192a722
Joost <hi@joost.im>
2025-03-15 14:03:09 +0100

    Initial

---
+++ src/main/kotlin/joost/im/angular_i18n/AngularI18nApplication.kt
@@ -0,0 +1,11 @@
+package joost.im.angular_i18n
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AngularI18nApplication
+
+fun main(args: Array<String>) {
+	runApplication<AngularI18nApplication>(*args)
+}
---
+++ src/test/kotlin/joost/im/angular_i18n/AngularI18nApplicationTests.kt
@@ -0,0 +1,13 @@
+package joost.im.angular_i18n
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AngularI18nApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

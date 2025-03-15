bb41de57b4cd9790d58c4316db3e245beee2de46
Joost <hi@joost.im>
2025-03-15 20:16:38 +0100

    some more

---
+++ src/main/kotlin/joost/im/angular_i18n/WebConfig.kt
@@ -0,0 +1,13 @@
+package joost.im.angular_i18n
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/").setViewName("/index.html")
+    }
+}
